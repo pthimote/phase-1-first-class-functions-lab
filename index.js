7f535206eb57ee5a7239d9ecf39fc5897adb4c30
Paul Thimote <pthimote@gmail.com>
2022-09-08 12:22:18 -0400

    commit tree 3efad846eebbcfaabd083057c445926b2fdea391

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array){
+    console.log(array[0]) 
+    return [array [0], array [1]]
+}
+
+function returnLastTwoDrivers(array) {
+    console.log(array[0])
+    return [array [2], array [3]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(array, callback) {
+    return callback(array)
+}
